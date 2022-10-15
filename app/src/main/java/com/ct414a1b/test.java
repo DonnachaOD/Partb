252ab21af3eca12c3e88a7bfa6891ec05fc3f46d
DonnachaOD <d.odwyer3@nuigalway.ie>
2022-10-15 17:46:08 +0100

    Edited formatting

--- app/src/main/java/com/ct414a1b/test.java
+++ app/src/main/java/com/ct414a1b/test.java
@@ -17,7 +17,6 @@ public class test {
         System.out.println(engineeringCourseY1);
     }
 
-
     private static ArrayList<Module> generateModules(ArrayList<Student> students) {
 
         ArrayList<Module> modules = new ArrayList<Module>();
@@ -40,26 +39,17 @@ public class test {
         return modules;
     }
 
-
     private static ArrayList<Student> generateStudents() {
 
         ArrayList<Student> students = new ArrayList<Student>();
-
-        Student s1 = new Student("Donnacha", new DateTime(2000, 06, 30,0,0),    19379026);
-        Student s2 = new Student("Luisa", new DateTime(2000, 10, 21,0,0),       19909090);
-        Student s3 = new Student("Sara", new DateTime(2000, 12, 23,0,0),        19379123);
-        Student s4 = new Student("Martha", new DateTime(2000, 01, 21,0,0),      19837321);
-        Student s5 = new Student("Ciaran", new DateTime(2000, 07, 07,0,0),      19434133);
-        Student s6 = new Student("Tadhg", new DateTime(2001, 01, 06,0,0),       19090080);
-        Student s7 = new Student("Aidan", new DateTime(2000, 06, 02,0,0),       19783747);
         
-        students.add(s1);
-        students.add(s2);
-        students.add(s3);
-        students.add(s4);
-        students.add(s5);
-        students.add(s6);
-        students.add(s7);
+        students.add(new Student("Donnacha", new DateTime(2000, 06, 30,0,0),    19379026));
+        students.add(new Student("Luisa", new DateTime(2000, 10, 21,0,0),       19909090));
+        students.add(new Student("Sara", new DateTime(2000, 12, 23,0,0),        19379123));
+        students.add(new Student("Martha", new DateTime(2000, 01, 21,0,0),      19837321));
+        students.add(new Student("Ciaran", new DateTime(2000, 07, 07,0,0),      19434133));
+        students.add(new Student("Tadhg", new DateTime(2001, 01, 06,0,0),       19090080));
+        students.add(new Student("Mark", new DateTime(2001, 9, 11,0,0),         19783747));
 
         return students;
     }
